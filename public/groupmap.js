ec2d3e97dacfc2493324307dddefcb0a46b9111f
Nope Nope <nope.nope@nope.nope>
2017-11-08 13:50:09 +0100

    code cleanup

--- public/groupmap.js
+++ public/groupmap.js
@@ -1,13 +1,16 @@
-var all_maps;
-var temp_marker;
-
-var leafletmap = L.map('map', {
-    minZoom: 2,
-    maxZoom: 7,
-    crs: L.CRS.Simple
-}).setView([-20, 100], 3);
-var marker_layer = new L.LayerGroup();
-var layerControl = L.control.layers(null, {'Toggle markers' : marker_layer}, {collapsed: false});
+// This is our magical global env
+var groupmap = {
+    // An array of map metadata
+    mapsList: undefined,
+    // The marker used to add more fluff
+    tempMarker: undefined,
+    // The leaflet map object
+    leafletMap: undefined,
+    // The leaflet markers layer
+    markersLayer: undefined,
+    // The leaflet control layer
+    controlLayer: undefined
+};
 
 function make_fluff_popup_text(fluff) {
     var html = '<b>'+fluff.name+'</b>';
@@ -27,7 +30,7 @@ function add_fluff(fluff, map_folder) {
         dataType: "json",
         data: fluff,
         success: function(data, textStatus) {
-            temp_marker.closePopup();
+            groupmap.tempMarker.closePopup();
             load_fluff_layer(map_folder);
         },
         error: function(data) {
@@ -39,19 +42,20 @@ function add_fluff(fluff, map_folder) {
     });
 }
 
-function show_fluff_marker(evt){
+function show_new_fluff_marker(evt){
+    var new_fluff = {};
     var lat = evt.latlng.lat;
     var lng = evt.latlng.lng;
 
-    if (typeof temp_marker !== 'undefined') {
-        temp_marker.unbindPopup();
-        temp_marker.remove();
+    if (typeof groupmap.tempMarker !== 'undefined') {
+        groupmap.tempMarker.unbindPopup();
+        groupmap.tempMarker.remove();
     }
 
-    temp_marker = L.marker([lat, lng]).addTo(marker_layer);
+    groupmap.tempMarker = L.marker([lat, lng]).addTo(groupmap.markersLayer);
 
-    var popupContent = "Who is that Fluff?"+
-        "<form id='add-fluff-form'>" +
+    var popupContent = "<b>Who is that Fluff?</b>"+
+        "<form id='add-fluff-form' style='width:15em'>" +
         "<input type='text' name='name' id='add-fluff-name-field' placeholder='Fluff name'>" +
         "<input type='text' name='info' id='add-fluff-info-field' placeholder='extra info of link'>" +
         "<input type='hidden' name='lng' id='add-fluff-lng-field' value='"+lng+"'>" +
@@ -59,39 +63,34 @@ function show_fluff_marker(evt){
         "<input type='submit' value='Save'>" +
         "</form>";
 
-    temp_marker.bindPopup(popupContent,{
-                    keepInView: true,
-                    }).openPopup();
+    groupmap.tempMarker.bindPopup(popupContent, {keepInView: true}).openPopup();
 
     $('#add-fluff-form').on('submit', function(e) {
         e.preventDefault();
-        var fluff = {};
-        fluff.name = $('#add-fluff-name-field').val();
-        fluff.info = $('#add-fluff-info-field').val();
-        fluff.lat = $('#add-fluff-lat-field').val();
-        fluff.lng = $('#add-fluff-lng-field').val();
-        add_fluff(fluff, evt.target.map_folder);
+        new_fluff.name = $('#add-fluff-name-field').val();
+        new_fluff.info = $('#add-fluff-info-field').val();
+        new_fluff.lat = $('#add-fluff-lat-field').val();
+        new_fluff.lng = $('#add-fluff-lng-field').val();
+        add_fluff(new_fluff, groupmap.map_folder);
     });
 }
 
 function load_fluff_layer(f) {
-    console.log('load fluff');
     $.ajax({
         url: "/list_fluff?map="+f,
         dataType: "json",
         success: function(data, textStatus) {
-            marker_layer.eachLayer(function (layer) {
-                    marker_layer.removeLayer(layer);
+            groupmap.markersLayer.eachLayer(function (layer) {
+                    groupmap.markersLayer.removeLayer(layer);
             });
             for (var i in data['fluff']) {
                 fluff = data['fluff'][i];
                 var popup_content = make_fluff_popup_text(fluff)
                 var marker = L.marker([fluff.lat, fluff.lng])
                 marker.bindPopup(popup_content);
-                marker.addTo(marker_layer);
+                marker.addTo(groupmap.markersLayer);
             }
-            marker_layer.addTo(leafletmap);
-            layerControl.addTo(leafletmap);
+            groupmap.markersLayer.addTo(groupmap.leafletMap);
         },
         error: function(data) {
             console.log(data.responseText);
@@ -102,9 +101,9 @@ function load_fluff_layer(f) {
     });
 }
 
-function get_map_metadata_from_folder(folder, all_maps) {
-    for (var i in all_maps) {
-        m = all_maps[i];
+function get_map_metadata_from_folder(folder) {
+    for (var i in groupmap.mapsList) {
+        m = groupmap.mapsList[i];
         if (m['folder'] == folder) {
             return m;
         }
@@ -140,37 +139,54 @@ function make_map_info_html(map_metadata) {
     return info_text.join(' - ');
 }
 
+function init_leafletmap(groupmap){
+    if (typeof groupmap.leafletMap === 'undefined') {
+        var map = L.map('map', {
+            minZoom: 2,
+            maxZoom: 7,
+            crs: L.CRS.Simple
+        }).setView([-20, 100], 3);
+        groupmap.leafletMap = map;
+        groupmap.markersLayer = new L.LayerGroup();
+        groupmap.controlLayer = L.control.layers(null, {'Toggle markers' : groupmap.markersLayer}, {collapsed: false});
+        groupmap.controlLayer.addTo(groupmap.leafletMap);
+    }
+    groupmap.leafletMap.eachLayer(function (layer) {
+            groupmap.leafletMap.removeLayer(layer);
+    });
+}
+
 function show_map(map_folder) {
-    leafletmap.eachLayer(function (layer) {
-            leafletmap.removeLayer(layer);
-    });
+    groupmap.map_folder = map_folder;
+    init_leafletmap(groupmap)
 
-    var tiles = L.tileLayer('/group_pics/'+map_folder+'/{z}/{y}/{x}.jpg', {
-        minzoom: 2,
-        maxzoom: 7,
+    // Adds the tiles/picture layer
+    L.tileLayer('/group_pics/'+map_folder+'/{z}/{y}/{x}.jpg', {
         noWrap: true,
-        attribution: make_map_info_html(get_map_metadata_from_folder(map_folder, all_maps))
-    });
+        attribution: make_map_info_html(get_map_metadata_from_folder(map_folder))
+    }).addTo(groupmap.leafletMap);
 
-    tiles.addTo(leafletmap);
-    leafletmap.map_folder = map_folder;
-    leafletmap.on('click', show_fluff_marker);
+    // Adds the markers layer
     load_fluff_layer(map_folder);
+
+    groupmap.leafletMap.on('click', show_new_fluff_marker);
 }
 
 function load_maps() {
-    $.ajax({
-        url: "/list_maps",
-        dataType: "json",
-        success: function(data, textStatus) {
-            all_maps = data['maps'];
-            show_map(all_maps[0]['folder']);
-        },
-        error: function(data) {
-            console.log(data.responseText);
-//            toastr.options.timeOut = 1500;
-//            toastr.options.positionClass= "toast-top-center";
-//            toastr.error(data.responseText);
-        }
-    });
+    if (typeof groupmap.mapsList === 'undefined'){
+        $.ajax({
+            url: "/list_maps",
+            dataType: "json",
+            success: function(data, textStatus) {
+                groupmap.mapsList = data['maps'];
+                show_map(groupmap.mapsList[0]['folder']);
+            },
+            error: function(data) {
+                console.log(data.responseText);
+    //            toastr.options.timeOut = 1500;
+    //            toastr.options.positionClass= "toast-top-center";
+    //            toastr.error(data.responseText);
+            }
+        });
+    }
 }
