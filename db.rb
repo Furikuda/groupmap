89433482d8014b6e1725c00061c2e42038346209
Nope Nope <nope.nope@nope.nope>
2018-05-20 12:57:13 +0200

    new Sequel

--- db.rb
+++ db.rb
@@ -2,21 +2,21 @@ module DBUtils
     require "logger"
     require "sequel"
 
-    Sequel::Model.plugin(:schema) 
     if not Object.const_defined?(:DB)
         DB = Sequel.sqlite 'db/fluff.sqlite', :loggers => [Logger.new($stderr)]
     end
 
     class Fluff < Sequel::Model(:fluff)
-        set_schema do
-            primary_key :id
-            String  :name, :unique => false, :empty => false
-            String  :map, :unique => false, :empty => false
-            Float   :lat, :unique => false, :empty => false
-            Float 	:lng, :unique => false, :empty => false
-            String  :info, :unique => false, :empty => true
+        unless DB.table_exists?(:fluff)
+            DB.create_table :fluff do
+                primary_key :id
+                String  :name, :unique => false, :empty => false
+                String  :map, :unique => false, :empty => false
+                Float   :lat, :unique => false, :empty => false
+                Float 	:lng, :unique => false, :empty => false
+                String  :info, :unique => false, :empty => true
+            end
         end
-        create_table unless table_exists?
     end
 
     def DBUtils.add_fluff(map, name, info, lat, lng)
