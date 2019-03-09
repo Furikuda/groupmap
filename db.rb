868001d9a7970b93eb3f0548063e3a06d44061a4
Nope Nope <nope.nope@nope.nope>
2019-03-09 16:41:47 +0100

    Fix DB stuff

--- db.rb
+++ db.rb
@@ -3,20 +3,24 @@ module DBUtils
     require "sequel"
 
     if not Object.const_defined?(:DB)
+        if not File.directory?('db')
+          Dir.mkdir('db')
+        end
         DB = Sequel.sqlite 'db/fluff.sqlite', :loggers => [Logger.new($stderr)]
     end
 
-    class Fluff < Sequel::Model(:fluff)
-        unless DB.table_exists?(:fluff)
-            DB.create_table :fluff do
-                primary_key :id
-                String  :name, :unique => false, :empty => false
-                String  :map, :unique => false, :empty => false
-                Float   :lat, :unique => false, :empty => false
-                Float 	:lng, :unique => false, :empty => false
-                String  :info, :unique => false, :empty => true
-            end
+    unless DB.table_exists?(:fluff)
+        DB.create_table :fluff do
+            primary_key :id
+            String  :name, :unique => false, :empty => false
+            String  :map, :unique => false, :empty => false
+            Float   :lat, :unique => false, :empty => false
+            Float 	:lng, :unique => false, :empty => false
+            String  :info, :unique => false, :empty => true
         end
+    end
+
+    class Fluff < Sequel::Model(:fluff)
     end
 
     def DBUtils.add_fluff(map, name, info, lat, lng)
